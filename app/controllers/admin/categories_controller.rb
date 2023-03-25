0e4a826bdcc5f584321061b0f250f5be0721e27f
AhernShaia <gemgt4gtii@gmail.com>
2023-03-25 20:21:30 +0800

    商品列表:新增,修改,刪除

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,7 @@
 class Admin::CategoriesController < Admin::BaseController
-
+    before_action :find_category_params, only:[:edit, :update, :destroy]
     def index
-        
+        @categories = Category.all
     end
 
     def new
@@ -11,14 +11,32 @@ class Admin::CategoriesController < Admin::BaseController
     def create
         @category = Category.new(category_params)
         if @category.save
-            redirect_to admin_categories_path
+            redirect_to admin_categories_path, notice: "新增成功"
         else
             render :new
         end
     end
 
+    def edit
+    end
+    def update
+        if @category.update(category_params)
+          redirect_to edit_admin_category_path(@category), notice: '已更新'
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @category.destroy
+        redirect_to admin_categories_path(@category), notice: "#{@category.name} 已刪除"
+    end
+
 
     private
+    def find_category_params
+        @category = Category.find(params[:id])
+    end
     def category_params
         params.require(:category).permit(:name)
     end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
+    validates :name, presence: true
     acts_as_paranoid
     acts_as_list
 
